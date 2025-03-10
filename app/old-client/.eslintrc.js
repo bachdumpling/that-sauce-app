161a25045826482fdec64c1d891ed61b307f01d5
Bach Le <lehoangbach7802@gmail.com>
2025-03-10 16:02:00 -0400

    Remove old client application files and configurations

--- app/old-client/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: "next/core-web-vitals",
-  rules: {
-    "no-unused-vars": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "react-hooks/exhaustive-deps": "warn",
-  },
-};
