047adae61714b8aa3665c4ba59536376df7ac39d
Bach Le <lehoangbach7802@gmail.com>
2025-04-21 21:18:03 -0400

    Remove task registration from trigger index as it is no longer needed

--- app/api/src/trigger/index.ts
+++ app/api/src/trigger/index.ts
@@ -1,16 +1,6 @@
 // Export all Trigger.dev tasks
-import { registerTasks } from "@trigger.dev/sdk/v3";
-
 export { imageAnalysisTask } from "./imageAnalysisTask";
 export { videoAnalysisTask } from "./videoAnalysisTask";
 export { projectAnalysisTask } from "./projectAnalysisTask";
 export { portfolioAnalysisTask } from "./portfolioAnalysisTask";
 export { scraperTask } from "./scraperTask";
-
-registerTasks({
-  imageAnalysisTask,
-  videoAnalysisTask,
-  projectAnalysisTask,
-  portfolioAnalysisTask,
-  scraperTask,
-});
